fd2b61219ce3346d649f94df1097c88109a24d10
Andree Villegas <andree.villegas.escom@gmail.com>
2020-10-20 09:36:04 -0500

    proyecto

---
+++ Back/PruebaConexion/src/pruebaconexion/PruebaConexion.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaconexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author giova
+ */
+public class PruebaConexion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String cadena = "jdbc:postgresql://localhost:5432/Prueba1";
+             String user ="postgres";
+             String pass = "holamundo";
+              try {
+                  Class.forName("org.postgresql.Driver");
+                  Connection conex = DriverManager.getConnection(cadena,user,pass);
+                  java.sql.Statement st = conex.createStatement();
+                  String sql =
+                      "select claves, nombre from prueba3 ";
+                  ResultSet result = st.executeQuery(sql);
+                  while(result.next()) {
+                      String usuario = result.getString("claves");
+                      String clave = result.getString("nombre");
+                      System.out.println("Clave: "+usuario + " nombre: " + clave);
+                  }
+                  result.close();
+                  st.close();
+                  conex.close();
+              } catch(Exception exc) {
+                  System.out.println("Errorx:"+exc.getMessage());
+              }
+    }
+    
+}
