9d8a81ec4768e3d1bdd14627665c0fb4eeaea8a6
Andree Villegas <andree.villegas.escom@gmail.com>
2020-10-20 10:16:24 -0500

    actualizacion

--- Back/PruebaConexion/src/pruebaconexion/conexionBBVA.java
+++ Back/PruebaConexion/src/pruebaconexion/conexionBBVA.java
@@ -46,7 +46,20 @@ public class conexionBBVA {
                       System.out.println("idcliente: "+ idcliente +" nombre: "+nombre +" apellidoPaterno: "+apellidoPaterno +  " apellidoMaterno: "+apellidoMaterno + " fechaNacimiento: "+ fechaNacimiento+ " sexo: "+sexo+" segmento: "+segmento+ " nacionalidad "+nacionalidad+ " rfc: "+rfc+" tipoID: "+tipoID+" numeroID: "+numeroID+" cuenta: "+cuenta+ "email: "+email);
                   }*/
                  
-                 //manager
+                 //restringido
+                 /* String sql =
+                      "select idcliente, nombre, sexo, segmento, numeroID, cuenta, email from clientes";
+                  ResultSet result = st.executeQuery(sql);
+                  while(result.next()) {
+                      String idcliente = result.getString("idcliente");
+                      String nombre = result.getString("nombre");
+                      String sexo = result.getString("sexo");
+                      String segmento = result.getString("segmento");
+                      String cuenta = result.getString("cuenta");
+                      
+                      System.out.println("idcliente: "+ idcliente +" nombre: "+nombre +" sexo: "+sexo+" segmento: "+segmento+" cuenta: "+cuenta);
+                  }*/
+                 //validador
                   String sql =
                       "select idcliente, nombre, apellidoPaterno, apellidoMaterno, fechaNacimiento, sexo, segmento, nacionalidad, rfc, tipoID, numeroID, cuenta, email from clientes";
                   ResultSet result = st.executeQuery(sql);
@@ -65,7 +78,7 @@ public class conexionBBVA {
                       String cuenta = result.getString("cuenta");
                       String email = result.getString("email");
                       
-                      System.out.println("idcliente: "+ idcliente +" nombre: "+nombre +" apellidoPaterno: "+apellidoPaterno +  " apellidoMaterno: "+apellidoMaterno + " fechaNacimiento: "+ fechaNacimiento+ " sexo: "+sexo+" segmento: "+segmento+ " nacionalidad "+nacionalidad+ " rfc: "+rfc+" tipoID: "+tipoID+" numeroID: "+numeroID+" cuenta: "+cuenta+ "email: "+email);
+                      System.out.println("idcliente: "+ idcliente +" nombre: "+nombre +" apellidoPaterno: "+apellidoPaterno.substring(0,3) +  " apellidoMaterno: "+apellidoMaterno.substring(0,3) + " fechaNacimiento: "+ fechaNacimiento.substring(0,3)+ " sexo: "+sexo+" segmento: "+segmento+ " nacionalidad "+nacionalidad.substring(0,3)+ " rfc: "+rfc.substring(0,3)+" tipoID: "+tipoID.substring(0,3)+" numeroID: "+numeroID.substring(0,3)+" cuenta: "+cuenta+ "email: "+email.substring(0,3));
                   }
                   result.close();
                   st.close();
